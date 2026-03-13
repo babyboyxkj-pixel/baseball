12f7b7d4384d013767fbaedcf9d705d44e6a3d59
babyboyxkj-pixel <babyboyx.kj@gmail.com>
2026-03-13 19:05:41 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://dmxkznmliiyffoltdcxd.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRteGt6bm1saWl5ZmZvbHRkY3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNTc2NzksImV4cCI6MjA1ODYzMzY3OX0.NUQgOp8NGHEZ0RDQ4sbuH7YCJpfYqF3UyEK3sPtu5-o'
+  SUPABASE_URL: 'https://eyunswxwvenhtxhkkntq.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5dW5zd3h3dmVuaHR4aGtrbnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzODg1MDgsImV4cCI6MjA4ODk2NDUwOH0.-xrnigivQgwkQJ-N0OhMhrgeGVlSRpPWXlQmhihRtaU'
 };
 
 window.sb = window.supabase.createClient(
